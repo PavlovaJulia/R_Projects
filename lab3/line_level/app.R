68fd29d45269f3f45c3ed7d6f44e0a4a6dc7ea6e
Julia <79782151900@mail.ru>
2018-12-12 20:12:13 +0300

    !

--- lab3/line_level/app.R
+++ lab3/line_level/app.R
@@ -33,23 +33,29 @@ server <- function(input, output) {
     m <- matrix(0, 1, 2) # мат ожидание
     if(det(matrix)>0){
       print(matrix)
+      # границы графика
       x1 <- matrix[1,1]+1
       x2 <- -matrix[1,1]-1
       y1 <- matrix[2,2]+1
       y2 <- -matrix[2,2]-1
+      
+      # координаты точек с подбираемым шагом
       x <- seq(x2, x1, length.out = 100)
       y <- seq(y2, y1, length.out = 100)
-      p <- matrix(0, length(x), length(y))
+      
+      p <- matrix(0, length(x), length(y)) # матрица плотностей
+      
       for(i in 1: length(x)){
         for(j in 1: length(y)){
-          point <- matrix(c(x[i], y[j]), 1, 2)
-          p[i,j] <- N(point, m, matrix)
+          point <- c(x[i], y[j])
+          p[i,j] <- N(point, m, matrix) # для каждой точки находим плотность
         }
       }
+      
       grafic <- F 
       for(i in seq(0.001, 0.1, input$p)){
         contour(x, y, p, levels = i, add = grafic, asp = 1)
-        grafic  = T # добавление новых солидных элипсов
+        grafic  = T # добавление новых элипсов к старым
       } 
     }
   
